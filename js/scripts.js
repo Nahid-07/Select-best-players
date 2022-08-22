fc103c8528bc8762659af440beef85a85e5c4eb1
Nahid-07 <nahidasif1998@gmail.com>
2022-08-22 15:47:53 +0600

    added total player cost function

--- js/scripts.js
+++ js/scripts.js
@@ -13,5 +13,21 @@ function getButtons(elementId){
         return;
     }
     listContainer.append(listItems);
+    
 }
+// common function
+function getInputFields(elementId){
+    const playerField = document.getElementById(elementId);
+    const playerInputString = playerField.value;
+    const playerInputNumber = parseInt(playerInputString);
+    return playerInputNumber
+}
+
+document.getElementById("btn-expense").addEventListener('click',function(){
+    const playerNumber = getInputFields('per-player-field')
+    const total = playerNumber*playerArray.length;
+    const totalPlayerCost = document.getElementById('total-player-cost');
+    totalPlayerCost.innerText = total
+})
+
 
