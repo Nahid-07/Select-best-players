256483635e5654602820ab66e1115b6628634e0f
Nahid-07 <nahidasif1998@gmail.com>
2022-08-22 19:23:53 +0600

    some common function added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 const playerArray = [];
+// common function
+function getInputFields(elementId){
+    const playerField = document.getElementById(elementId);
+    const playerInputString = playerField.value;
+    const playerInputNumber = parseInt(playerInputString);
+    return playerInputNumber
+}
+// common function 2
+function covertTextToNumber(elementId){
+    const CostFieldString = getInputFields(elementId);
+    const CostFieldInput = parseInt(CostFieldString);
+    return CostFieldInput
+}
 function getButtons(elementId){
     const PlayerName = elementId.parentNode.children[0].innerText;
     const playerObject = {
@@ -13,21 +26,22 @@ function getButtons(elementId){
         return;
     }
     listContainer.append(listItems);
-    
 }
-// common function
-function getInputFields(elementId){
-    const playerField = document.getElementById(elementId);
-    const playerInputString = playerField.value;
-    const playerInputNumber = parseInt(playerInputString);
-    return playerInputNumber
-}
-
 document.getElementById("btn-expense").addEventListener('click',function(){
     const playerNumber = getInputFields('per-player-field')
     const total = playerNumber*playerArray.length;
+    const totalNumber = parseInt(total)
     const totalPlayerCost = document.getElementById('total-player-cost');
-    totalPlayerCost.innerText = total
+    totalPlayerCost.innerText = totalNumber;
+    console.log(totalNumber)
 })
-
-
+document.getElementById('btn-over-all-cost').addEventListener('click',function(){
+    const mangerCostFieldInput = covertTextToNumber('manger-cost-field');
+    const coachCostFieldInput = covertTextToNumber('coach-cost-field')
+    const totalPlayerCosElement = document.getElementById('total-player-cost');
+    const totalPlayerCosString = totalPlayerCosElement.innerText;
+    const totalPlayerCostNumber = parseInt(totalPlayerCosString);
+    const costOfManagerAndCoachPlayer = mangerCostFieldInput + coachCostFieldInput + totalPlayerCostNumber;
+    const overAllTotalCost = document.getElementById('total-cost-of-all');
+    overAllTotalCost.innerText = costOfManagerAndCoachPlayer;
+})
